62ec7a9d343cadff03cdba90c83cfed1b733ed0d
takuzo <takuya.koiso.s@gmail.com>
2024-01-13 06:02:30 +0900

    rubocop

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,4 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 # for bootstrap
-Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+Rails.application.config.assets.precompile += %w[bootstrap.min.js popper.js]
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Items", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Items', type: :request do
+  describe 'GET /index' do
     it 'renders a successful response' do
       get items_url
       expect(response).to be_successful
-      expect(response).to have_http_status "200"
-      end
+    end
   end
 end
