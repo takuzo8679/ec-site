a59417a29890c45610fd4bc30acc15d96b49cd39
takuzo <takuya.koiso.s@gmail.com>
2024-01-13 04:40:15 +0900

    bootstrapの導入と商品一覧ページ

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
+//= require jquery3
+//= require popper
+//= require bootstrap
 import "@hotwired/turbo-rails"
 import "./controllers"
-import * as bootstrap from "bootstrap"
+// import * as bootstrap from "bootstrap"
