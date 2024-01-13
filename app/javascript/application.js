cbf1be67d95a3a80f903642240bdaf8e00459558
takuzo <takuya.koiso.s@gmail.com>
2024-01-14 04:33:34 +0900

    bootstrapの設定修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Entry point for the build script in your package.json
 //= require jquery3
 //= require popper
-//= require bootstrap
+//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "./controllers"
 // import * as bootstrap from "bootstrap"
