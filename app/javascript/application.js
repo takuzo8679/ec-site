814571a33e6239f8f950116aaa3ef372fd9baa1f
takuzo <takuya.koiso.s@gmail.com>
2024-01-21 19:51:51 +0900

    Revert "bootstrapの導入と商品一覧ページ"
    This reverts commit a59417a29890c45610fd4bc30acc15d96b49cd39.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,4 @@
 // Entry point for the build script in your package.json
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
 import "@hotwired/turbo-rails"
 import "./controllers"
-// import * as bootstrap from "bootstrap"
+import * as bootstrap from "bootstrap"
