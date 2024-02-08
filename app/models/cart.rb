fff91f7bae16da7f9a351fb2be8d5a7bd8803b9e
takuzo <takuya.koiso.s@gmail.com>
2024-02-09 05:51:33 +0900

    cart, cart_itemsモデルの作成

---
+++ app/models/cart.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: carts
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :delete_all
+  has_many :items, through: :cart_items
+end
---
+++ app/models/cart_item.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: cart_items
+#
+#  id         :bigint           not null, primary key
+#  quantity   :integer          default(0), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  cart_id    :bigint           not null
+#  item_id    :bigint           not null
+#
+# Indexes
+#
+#  index_cart_items_on_cart_id  (cart_id)
+#  index_cart_items_on_item_id  (item_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (cart_id => carts.id)
+#  fk_rails_...  (item_id => items.id)
+#
+class CartItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :item
+end
